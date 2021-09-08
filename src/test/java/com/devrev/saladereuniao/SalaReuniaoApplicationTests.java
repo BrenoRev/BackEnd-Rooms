641f51f48438192bd25dcbde571d10fc6d45c066
BrenoRev <silvabreno462@gmail.com>
2021-09-08 18:29:46 -0300

    Startíng project

---
+++ src/test/java/com/devrev/saladereuniao/SalaReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrev.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
