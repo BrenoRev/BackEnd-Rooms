b0be959505f6a671668ab985751f3330bebd3f73
BrenoRev <silvabreno462@gmail.com>
2021-09-08 19:45:40 -0300

    Criando o handler exception dos erros internos

--- src/main/java/com/devrev/saladereuniao/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/devrev/saladereuniao/exceptions/GlobalExceptionHandler.java
@@ -17,4 +17,9 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
 	}
 	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request){
+		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
